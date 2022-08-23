3f1bde46aba86488d4fafd66cc5035e553f37a2f
Siwakorn <siwakorn099@gmail.com>
2022-08-23 11:57:36 +0700

    update route add product

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::get('about', [HomeController::class,'about']);
 Route::get('service', [HomeController::class,'service']);
 Route::get('contact', [HomeController::class,'contact']);
 Route::get('login', [HomeController::class,'login']);
+Route::get('product', function(){
+    return"product page";
+});
